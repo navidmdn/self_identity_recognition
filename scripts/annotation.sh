131478bcec5326f023fba9baf8431f0ba2fa389a
Navid Madani <navid.madani@tap30.net>
2020-06-01 04:14:49 +0430

    add port to annotation server

--- scripts/annotation.sh
+++ scripts/annotation.sh
@@ -3,4 +3,4 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-PRODIGY_PORT=27654 prodigy ner.manual $1 en_core_web_sm ../data/prodigy/descriptions.jsonl --label IDENTITY --patterns ../data/prodigy/patterns.jsonl
+PRODIGY_PORT=$2 prodigy ner.manual $1 en_core_web_sm ../data/prodigy/descriptions.jsonl --label IDENTITY --patterns ../data/prodigy/patterns.jsonl
